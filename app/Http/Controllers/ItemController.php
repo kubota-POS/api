6df0373eeea4279a610f01d4dcf930bc9c85f143
ApoloKuu <yeswamyee.yyy@gmail.com>
2022-09-20 15:17:22 +0630

    []

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Validator;
 use App\Models\ItemModel;
 use App\Imports\PriceImport;
 use App\Models\SecondItem;
@@ -14,29 +13,31 @@ use App\Models\CategoryModel;
 use App\HttpResponse\ApiResponse;
 use Maatwebsite\Excel\Facades\Excel;
 use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\Validator;
 
 ini_set('max_execution_time', '900000');
 
 class ItemController extends Controller
 {
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->middleware('jwt.verify', ['except' => ['importPrice']]);
         $this->middleware('license', ['except' => ['importPrice']]);
     }
 
-    public function index () {
+    public function index()
+    {
         try {
             $items = ItemModel::with(['category'])->get();
-            $response = ApiResponse::Success($items, 'get all items');
-            return response()->json($response['json'], $response['status']);
+            return $this->success($items, 'get all items');
         } catch (QueryException $e) {
-            $response = ApiResponse::Unknown('someting was wrong');
-            return response()->json($response['json'], $response['status']);
+            return $this->unknown();
         }
     }
 
-    public function create (Request $request) {
+    public function create(Request $request)
+    {
         $input = $request->only(['category_id', 'code', 'eng_name', 'mm_name', 'model', 'qty', 'price', 'percentage', 'location', 'active']);
 
         $validator = Validator::make($input, [
@@ -45,62 +46,55 @@ class ItemController extends Controller
         ]);
 
         if ($validator->fails()) {
-            $response = ApiResponse::BedRequest($validator->errors()->first());
-            return response()->json($response['json'], $response['status']);
+            return $this->unprocess($validator->errors()->first());
         }
 
-         try {
-             $newItem = ItemModel::create($input);
-             $response = ApiResponse::Success($newItem, 'new item is created');
-             return response()->json($response['json'], $response['status']);
-         } catch (QueryException $e) {
-             $response = ApiResponse::Unknown('someting was wrong');
-             return response()->json($response['json'], $response['status']);
+        try {
+            $newItem = ItemModel::create($input);
+            return $this->success($newItem, 'new item is created', 201);
+        } catch (QueryException $e) {
+            return $this->unknown();
         }
     }
 
-    public function detail (Request $request) {
+    public function detail(Request $request)
+    {
         $id = $request->id;
 
         try {
             $item = ItemModel::with(['category'])->find($id);
 
-            if($item) {
-                $response = ApiResponse::Success($item, 'get item detail');
+            if ($item) {
+                return $this->success($item, 'get item detail');
             } else {
-                $response = ApiResponse::NotFound('item does not found');
+                return $this->notFound('item does not found');
             }
-
-            return response()->json($response['json'], $response['status']);  
-
-        } catch(QueryException $e) {
-            $response = ApiResponse::Unknown('someting was wrong');
-            return response()->json($response['json'], $response['status']);   
+        } catch (QueryException $e) {
+            return $this->unknown();
         }
     }
 
-    public function update (Request $request) {
+    public function update(Request $request)
+    {
         $id = $request->id;
         $input = $request->only(['category_id', 'code', 'eng_name', 'mm_name', 'model', 'qty', 'price', 'percentage', 'fix_amount', 'location', 'active']);
 
         try {
             $item = ItemModel::with(['category'])->find($id);
 
-            if(!$item) {
-                $response = ApiResponse::NotFound('item is not found');
+            if (!$item) {
+                return $this->notFound('item is not found');
             } else {
                 $item->update($input);
-                $response = ApiResponse::Success($item, 'item is updated');
+                return $this->success($item, 'item is updated', 201);
             }
-
-            return response()->json($response['json'], $response['status']);
         } catch (QueryException $e) {
-            $response = ApiResponse::Unknown('someting was wrong');
-            return response()->json($response['json'], $response['status']);
+            return $this->unknown();
         }
     }
 
-    public function updatePercentage(Request $request) {
+    public function updatePercentage(Request $request)
+    {
         $input = $request->only(['amount', 'type']);
 
         $validator = Validator::make($input, [
@@ -109,59 +103,54 @@ class ItemController extends Controller
         ]);
 
         if ($validator->fails()) {
-            $response = ApiResponse::BedRequest($validator->errors()->first());
-            return response()->json($response['json'], $response['status']);
+            return $this->unprocess($validator->errors()->first());
         }
 
         try {
             $newItem = new ItemModel;
-            if($input['type'] ==='increment') {
+            if ($input['type'] === 'increment') {
                 $newItem->increment('percentage', $request->amount);
             }
 
-            if($input['type'] === 'decrement') {
+            if ($input['type'] === 'decrement') {
                 $newItem->decrement('percentage', $request->amount);
             }
 
             $newItem->update();
 
-            $response = ApiResponse::Success($newItem, "item's percentage are updated");
-            return response()->json($response['json'], $response['status']);
+            return $this->success($newItem, "item's percentage are updated", 201);
         } catch (QueryException $e) {
-            $response = ApiResponse::Unknown('someting was wrong');
-            return response()->json($response['json'], $response['status']);
+            return $this->unknown();
         }
     }
 
-    public function delete(Request $request) {
+    public function delete(Request $request)
+    {
         $id = $request->id;
 
         try {
             $item = ItemModel::find($id);
 
-            if(!$item) {
-                $response = ApiResponse::NotFound('item is not found');
+            if (!$item) {
+                return $this->notFound('item is not found');
             } else {
                 $item->delete();
-                $response = ApiResponse::Success($item, 'item is deleted');
+                return $this->success($item, 'item is deleted');
             }
-
-            return response()->json($response['json'], $response['status']);
-
-        } catch(QueryException $e) {
-            $response = ApiResponse::Unknown('someting was wrong');
-            return response()->json($response['json'], $response['status']);
+        } catch (QueryException $e) {
+            return $this->unknown();
         }
     }
 
-    public function importPrice() {
+    public function importPrice()
+    {
         $PricePath = storage_path('../database/seeders/xlsx/prices.xlsx');
         $rows = Excel::toArray(new PriceImport, $PricePath);
 
         $updateList = [];
         $createItemList = [];
 
-        foreach($rows[0] as $row) {
+        foreach ($rows[0] as $row) {
             $item = [
                 'code' => $row['material_code'],
                 'price' => $row['price']
@@ -169,7 +158,7 @@ class ItemController extends Controller
 
             $checkItem = ItemModel::where('code', $item['code'])->get()->first();
 
-            if($checkItem) {
+            if ($checkItem) {
                 $updateItem = ItemModel::where('code', $item['code'])->update([
                     'price' => $item['price'],
                 ]);
@@ -177,15 +166,15 @@ class ItemController extends Controller
                 array_push($updateList, $item);
             } else {
                 $createItem = [
-                    'category_id' => 1, 
-                    'code' => $item['code'], 
-                    'eng_name' => null, 
-                    'mm_name' => null, 
-                    'model' => null, 
-                    'qty' => 0, 
-                    'price' => $item['price'], 
-                    'percentage' => null, 
-                    'location' => null, 
+                    'category_id' => 1,
+                    'code' => $item['code'],
+                    'eng_name' => null,
+                    'mm_name' => null,
+                    'model' => null,
+                    'qty' => 0,
+                    'price' => $item['price'],
+                    'percentage' => null,
+                    'location' => null,
                     'active' => true
                 ];
 
@@ -205,38 +194,34 @@ class ItemController extends Controller
             ]
         ], 'Success');
         return response()->json($response['json'], $response['status']);
-
     }
 
-    public function export() 
+    public function export()
     {
-       return Excel::download(new ItemExport, 'Items.xlsx');
+        return Excel::download(new ItemExport, 'Items.xlsx');
     }
 
-    public function deleteMultiple(Request $request) {
-        $ids=$request->data;
+    public function deleteMultiple(Request $request)
+    {
+        $ids = $request->data;
         $input = $request->only(['data']);
-        
+
         $validator = Validator::make($input, [
             "data" => "required"
         ]);
 
         if ($validator->fails()) {
-            $response = ApiResponse::BedRequest($validator->errors()->first());
-            return response()->json($response['json'], $response['status']);
+            return $this->unprocess($validator->errors()->first());
         }
 
         try {
             $deleteditem = ItemModel::find($ids);
             $ids = $input['data'];
             $item = ItemModel::whereIn('id', $ids)->delete();
-        
-            $response = ApiResponse::Success($deleteditem, 'items are deleted');
-            return response()->json($response['json'], $response['status']);
 
-        } catch(QueryException $e) {
-            $response = ApiResponse::Unknown('someting was wrong');
-            return response()->json($response['json'], $response['status']);
+            return $this->success($deleteditem, 'items are deleted');
+        } catch (QueryException $e) {
+            return $this->unknown();
         }
     }
 }
